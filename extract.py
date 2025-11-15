e2deedc3924e6722916d6b8d8482385bd8b0dda6
Praveen Naradwar <naradwarpraveen1997@gmail.com>
2025-11-15 14:00:48 +0530

    testing5

--- extract.py
+++ extract.py
@@ -1,3 +1,13 @@
-
+import pandas as pd
 
 print("Extraxt data from mysql database")
+
+data={
+
+    "id":[11,12,13],
+    "name":['komal','radha','nikita'],
+    "address":['pune','mumbai','pune']
+}
+
+df=pd.DataFrame(data)
+print(df)
