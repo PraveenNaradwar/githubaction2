9484424da13c806ad86b2217d927f3aaf112b141
Praveen Naradwar <naradwarpraveen1997@gmail.com>
2026-02-06 13:31:00 +0530

    testingjob-2

--- extract.py
+++ extract.py
@@ -1,4 +1,13 @@
 
-
+import pandas as pd
 
 print("extract data from mysql database")
+
+data={
+    "userid":[101,102,103],
+    "name":['pooja','anil','sunil'],
+    "age":[25,26,27]
+}
+
+df=pd.DataFrame(data)
+print(df)
